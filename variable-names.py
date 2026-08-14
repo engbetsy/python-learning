4e48fd9214aef2d281f9e0e5d32948af885787a7
Lucien Makutano <lmakutano@microsoft.com>
2026-08-15 00:51:17 +0300

    initial files

---
+++ variable-names.py
@@ -0,0 +1,12 @@
+myvar = "Muthomi"
+my_var = "Mitchell"
+_my_var = "Makena"
+myVar = "Stella"
+MYVAR = "Kawira"
+myvar2 = "Betsy"
+
+myVariableName = "Kenya" # camel case each word except the first starts with a capital letter
+
+MyVariableName = "Kenya" # pascal case each word starts with a capital letter
+
+my_variable_name = "Kenya" # snake case each word is separated by an underscore character
---
+++ variables.py
@@ -0,0 +1,23 @@
+x = 5
+y = "muthomi"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "kawira" # x is now of type str
+print(x)
+
+#casting variables by specifying the data type of the variable
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+age=int("20")
+print(age)   # age will be 20
+x=int(10.5)
+print(x)  # x will be 10
+x=float(10)
+print(x)  # x will be 10.5
+
+
+
