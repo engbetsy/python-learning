6f0c5b82034523e830d682a1815b1c370051b3bc
engbetsy <betsykawira52@gmail.com>
2026-08-17 17:28:38 +0300

    initial files

---
+++ bool.py
@@ -0,0 +1,17 @@
+#bool
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+  #evaluate values and variables
+
+  x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
