6c4245f3e223f57061c1e08cbcfcabf94f4ac38c
engbetsy <betsykawira52@gmail.com>
2026-08-26 12:55:49 +0300

    initial files

---
+++ arithmetic-operator.py
@@ -0,0 +1,34 @@
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#Operator	Name	Example	
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y
+
+#division in python
+x = 12
+y = 5
+
+print(x / y)  #/ - Division (returns a float)
+
+#Floor division always returns an integer.
+#It rounds DOWN to the nearest integer
+
+
+x = 12
+y = 5
+
+print(x // y)
